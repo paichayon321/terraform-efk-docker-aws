600efa7274468bf7ff3c96e9005a74effa6030b1
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-06 08:09:50 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-bucket-poc-3436"
-    key    = "terraform.sonarqube-tfstate"
+    key    = "terraform.efk-tfstate"
     region = "ap-southeast-1"
   }
 }
@@ -19,16 +19,16 @@ resource "aws_instance" "default" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
-  user_data = "${file("run-sonarqube.sh")}"
+  user_data = "${file("run.sh")}"
   instance_type          = var.instance_type
 
   tags = {
-    Name = "SonarQube-Terraform"
+    Name = "efk-Terraform"
   }
 }
 
 resource "aws_security_group" "default" {
-  name = "sonarqube-terraform-sg"
+  name = "efk-terraform-sg"
   ingress {
     protocol  = "TCP"
     from_port = 22
@@ -37,12 +37,26 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
-    from_port   = 9000
-    to_port     = 9000
+    from_port   = 9200
+    to_port     = 9200
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
+    ingress {
+    from_port   = 9300
+    to_port     = 9300
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   egress {
     from_port       = 0
     to_port         = 0
